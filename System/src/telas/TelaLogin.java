c167285e096921a1ceed1ab0154387b5198bad11
Cauã Gabriel Barbosa <110111422+Caua-Gabriel@users.noreply.github.com>
2023-05-16 19:24:56 -0300

    criacao cardapio

--- System/src/telas/TelaLogin.java
+++ System/src/telas/TelaLogin.java
@@ -120,9 +120,7 @@ public class TelaLogin extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnEntrarActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
